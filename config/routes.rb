ecac8a8902ca03fb516b542aba2da239979f59c9
moshesegelman <moshesegelman41@gmail.com>
2020-07-17 17:04:45 -0400

    adds api routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,38 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    # get "/users" => "users#index"
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+ 
+    get "/subjects" => "subjects#index"
+    get "/subjects/:id" => "subjects#show"
+    # post "/subjects" => "subjects#create"
+    # patch "/subjects/:id" => "subjects#update"
+    # delete "/subjects/:id" => "subjects#destroy"
+     
+    # get "/channels" => "channels#index"
+    get "/channels/:id" => "channels#show"
+    post "/channels" => "channels#create"
+    patch "/channels/:id" => "channels#update"
+    delete "/channels/:id" => "channels#destroy"
+ 
+    # get "/messages" => "messages#index"
+    get "/messages/:id" => "messages#show"
+    post "/messages" => "messages#create"
+    # patch "/messages/:id" => "messages#update"
+    delete "/messages/:id" => "messages#destroy"
+ 
+    get "/conversations" => "conversations#index"
+    get "/conversations/:id" => "conversations#show"
+    post "/conversations" => "conversations#create"
+     # patch "/conversations/:id" => "conversations#update"
+    delete "/conversations/:id" => "conversations#destroy"
+  
+ 
+    post "/sessions" => "sessions#create"
+  end
 end
