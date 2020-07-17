ba4c692370d88ff6367ac14c6d47cb1accb13792
moshesegelman <moshesegelman41@gmail.com>
2020-07-17 17:59:21 -0400

    adds messages controller actions and views

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,34 @@
 class Api::MessagesController < ApplicationController
+
+  before_action :authenticate_user, except:[:show] 
+
+  def show
+    @message = Message.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @message = Message.new(
+      text: params[:text],
+      channel_id: params[:channel_id],
+      conversation_id: params[:conversation_id],
+      user_id: current_user.id
+    )
+
+    if @message.save
+      render 'show.json.jb'
+    else
+      render json: {error: @message.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    if current_user.id == @message.user_id
+    @message.destroy
+    render json: {message: "message has been deleted"}
+    else
+      render json: {error: @message.errors.full_messages}, status: :unauthorized
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  validates :text, presence: true
+
+  belongs_to :user
+  belongs_to :conversation, optional: true 
+  belongs_to :channel, optional: true 
 end
