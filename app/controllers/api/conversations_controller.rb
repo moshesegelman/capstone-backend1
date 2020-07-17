631521b61bfe943105f8088536582b730f35a59b
moshesegelman <moshesegelman41@gmail.com>
2020-07-17 18:26:08 -0400

    fixes seeds file adds conversation controller and views

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,2 +1,32 @@
 class Api::ConversationsController < ApplicationController
+  before_action :authenticate_user 
+
+  def index
+    @conversations = current_user.conversations
+    render 'index.json.jb'
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @conversation = Conversation.new(
+      recipient_id: params[:recipient_id],
+      sender_id: current_user.id
+    )
+
+    if @conversation.save
+      render 'show.json.jb'
+    else
+      render json: {error: @conversation.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @conversation = Conversation.find(params[:id])
+    @conversation.destroy
+    render json: {message: "conversation has been deleted"}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ User.create!([
   { first_name: "jack", last_name: "smith", username: "jsmith", email: "jack@gmail.com", password: "password", password_confirmation: "password"},
   { first_name: "jill", last_name: "parky", username: "jparky", email: "jill@gmail.com", password: "password", password_confirmation: "password"}
 ])
+user1 = User.first
+user2 = User.second
+user3 = User.third 
 
 Subject.create!([ 
   {name:"math", image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQjx0t8eLaPHrPJiVSvKjwr-OhWd2s4b75NgQ&usqp=CAU"},
@@ -11,22 +14,22 @@ Subject.create!([
 ])
 
 Channel.create!([ 
-  {name:"pre-calc", details: "preparing for test that involves these methods", user_id: 1, subject_id: 1},
-  {name:"bio", details: "studying the human cells", user_id: 2, subject_id: 2},
-  {name:"shakespear", details: "going over Julias Ceasar", user_id: 3, subject_id: 3}
+  {name:"pre-calc", details: "preparing for test that involves these methods", user_id: user1.id, subject_id: 1},
+  {name:"bio", details: "studying the human cells", user_id: user2.id, subject_id: 2},
+  {name:"shakespear", details: "going over Julias Ceasar", user_id: user3.id, subject_id: 3}
 ])
 
 Conversation.create!([ 
-  {sender_id: 1,recipient_id: 2},
-  {sender_id: 2,recipient_id: 3},
-  {sender_id: 3,recipient_id: 1}
+  {sender_id: user1.id,recipient_id: user2.id},
+  {sender_id: user2.id,recipient_id: user3.id},
+  {sender_id: user3.id,recipient_id: user1.id}
 ])
 
 Message.create!([ 
-  {text:"hey what methods are you going over?", user_id: 2, channel_id: 1, },
-  {text:"hey im going over white blood cells and their diseases", user_id: 3, channel_id: 2, },
-  {text:"i just finished with that play in class hmu", user_id: 1, channel_id: 3, },
-  {text:"hey im going over this method wanna work together", user_id: 1,  conversation_id:1},
-  {text:"im going over red blood cells but maybe we can help eachother out", user_id:2,  conversation_id: 2},
-  {text:"hey you just did it in school if you can help me out that would be great", user_id: 3,  conversation_id: 3},
+  {text:"hey what methods are you going over?", user_id: user2.id, channel_id: 1, },
+  {text:"hey im going over white blood cells and their diseases", user_id: user3.id, channel_id: 2, },
+  {text:"i just finished with that play in class hmu", user_id: user1.id, channel_id: 3, },
+  {text:"hey im going over this method wanna work together", user_id: user1.id,  conversation_id:1},
+  {text:"im going over red blood cells but maybe we can help eachother out", user_id:user2.id,  conversation_id: 2},
+  {text:"hey you just did it in school if you can help me out that would be great", user_id: user3.id,  conversation_id: 3},
 ])
