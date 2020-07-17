aa11d65e46cc1de2256ac4bb8cf47fdc130729af
moshesegelman <moshesegelman41@gmail.com>
2020-07-17 17:10:47 -0400

    adds authentication and user crud actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,58 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate_user, except:[:create]
+
+  def show
+    @user = User.find(params[:id])
+    if current_user == @user
+      render "show.json.jb"
+    end
+  end
+
+  def create
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      user_name: params[:user_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unauthorized
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user
+      @user = current_user
+      @user.first_name = params[:first_name] || @user.first_name
+      @user.last_name = params[:last_name] || @user.last_name
+      @user.user_name = params[:user_name] || @user.user_name
+      @user.email = params[:email] || @user.email
+      if params[:password]
+        @user.password = params[:password]
+        @user.password_confirmation = params[:password_confirmation]
+      end
+    end
+
+    if @user.save
+      render 'show.json.jb'
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user == @user
+      @user.destroy
+      render json: {message: "user has been deleted"}
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unauthorized
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  has_many :channels
+  has_many :messages , dependant: :destroy
+  
+  def conversations
+    Conversation.where("sender_id = ? OR recipient_id = ?", id, id)
+  end
 end
