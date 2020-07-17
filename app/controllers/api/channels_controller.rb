da882c299377c068303ff26be89e6d65be941cfb
moshesegelman <moshesegelman41@gmail.com>
2020-07-17 17:53:15 -0400

    adds channel contorller actions and views

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,2 +1,50 @@
 class Api::ChannelsController < ApplicationController
+
+  before_action :authenticate_user, except:[:show] 
+
+  def show
+    @channel = Channel.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @channel = Channel.new(
+      name: params[:name],
+      details: params[:details],
+      subject_id: params[:subject_id],
+      user_id: current_user.id
+    )
+    if @channel.save
+      render 'show.json.jb'
+    else
+      render json: {error: @channel.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update  
+    @channel = Channel.find(params[:id])
+    if current_user.id == @channel.user_id
+      @channel.name = params[:name] || @channel.name
+      @channel.details = params[:details] || @channel.details
+    
+      if @channel.save
+        render 'show.json.jb'
+      else
+        render json: {error: @channel.errors.full_messages}, status: :unprocessable_entity
+      end
+    else
+      render json: {error: @channel.errors.full_messages}, status: :unauthorized  
+    end 
+  end
+
+  def destroy
+    @channel = Channel.find(params[:id])
+    if current_user.id == @channel.user_id
+      @channel.destroy
+      render json: {message: "channel has been deleted"}
+    else
+      render json: {error: @channel.errors.full_messages}, status: :unauthorized  
+    end
+  end
+
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,7 @@
 class Channel < ApplicationRecord
+  validates :name, presence: true
+
+  belongs_to :subject
+  belongs_to :user
+  has_many :messages, dependent: :destroy
 end
